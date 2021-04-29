9910acb8d933d3d6accce762907b13807e7526ec
Mahirweyves <yvesmahirwe1@gmail.com>
2021-04-28 23:12:52 -0700

    fixed config & added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,29 @@
 Rails.application.routes.draw do
+
+  # mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
+
+  # resources :contacts, only: [:new, :create] 
+  # post "/product/message" => "products#email_product", as: :email_product
+  #  get "products/terms_and_conditions", :to => "products#terms_and_conditions"
+  #  get "products/privacy_policy", :to => "products#privacy_policy"
+  # resources :products do
+  #   resources :comments
+    
+
+  # end
+
+  # get '/search' => 'products#search', :as => 'search_product' 
+
+  
+  devise_for :users
+   get "users/products/terms_and_conditions", :to => "products#terms_and_conditions"
   root to: 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/cancel' => 'devise/registrations#cancel'
+  end 
+  # resources :categories
+
 end
